3266b5e6bafcde6f4a065e961e9eb9d71544a18e
devnamz <engineer.soft1990@gmail.com>
2020-04-22 00:51:33 +0530

    Added Basic Navigation to Shop Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HomePageNew = (props) => {
+  console.log(props); 
+  return ( 
+      <div>
+        <h1>HATS PAGE</h1>
+        <Link to="/topics">Topic</Link>
+        <button onClick= {() => props.history.push('topics')}>Topics</button>
+      </div>
+    );
+  }
 function App() {
   return (
     <div>
-      <HomePage/>
+        <Route exact path="/" component={HomePage}/>
     </div>
   );
 }
