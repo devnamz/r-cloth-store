aa6424ede2b81f2aa20860e497a399c8ec006db4
devnamz <engineer.soft1990@gmail.com>
2020-04-25 02:08:02 +0530

    Integrating Header component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/shop" component={ShopPage}/>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/shop" component={ShopPage}/>
+        </Switch>
     </div>
   );
 }
